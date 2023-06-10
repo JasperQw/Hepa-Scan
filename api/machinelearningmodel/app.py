b2ff5b5e5ba77e4b3fbfca8778fc4dda0004f6b2
Ng Ching Wei <22004844@siswa.um.edu.my>
2023-06-10 23:37:11 +0800

    Revert "Delete"
    This reverts commit 1d1e61d05f4e597e36644e0e0c6d294e53a4c45f.

---
+++ api/machinelearningmodel/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import joblib
+from flask_cors import CORS
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>API is working..</h1>"
+
+@app.route('/etcModel', methods=['GET', 'POST'])
+def predict():
+    loaded_model = joblib.load('etc_model.pkl')
+    input_data = request.get_json(force=True)['data']
+    for index, data in enumerate(input_data):
+        input_data[index] = float(data)
+
+    input_data = pd.DataFrame([input_data], columns=[
+                              'Age', 'Gender', 'TB', 'DB', 'Alkphos', 'Sgpt', 'Sgot', 'TP', 'ALB', 'A/G_Ratio'])
+    prediction = loaded_model.predict(input_data)
+    message = ""
+
+    if (prediction == 1):
+        message = "Patient is likely to have liver disease"
+    else:
+        message = "Patient is unlikely to have liver disease"
+    response = {
+        'prediction': prediction.tolist(),
+        'message': message
+    }
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
